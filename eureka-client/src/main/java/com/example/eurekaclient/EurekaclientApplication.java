e20ddf7bcde6038f7c38cc56aaafb00f542dc1f4
maoxiaoyao222 <1135892360@qq.com>
2020-09-07 23:39:29 +0800

    cloud study project

---
+++ eureka-client/src/main/java/com/example/eurekaclient/EurekaclientApplication.java
@@ -0,0 +1,22 @@
+package com.example.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EurekaclientApplication {
+
+  @RequestMapping("/hello")
+  public String hello() {
+    return "hello world!";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaclientApplication.class, args);
+  }
+}
